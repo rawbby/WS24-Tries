c55037a516c064988069d8627edd716241471694
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-01-26 21:10:29 +0100

    hopefully fix MSVC ASSERT

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -1,1 +1,6 @@
 target_link_libraries(${TARGET_NAME} PRIVATE tries)
+
+if (MSVC)
+    # ensure __VA_OPT__ is supported (C++20 feature)
+    target_compile_options(${TARGET_NAME} PRIVATE "/Zc:preprocessor")
+endif ()
